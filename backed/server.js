97bd5f4be256c077c3be91e555e363395a2c2c13
Shreyash <cyberninjax45@gmail.com>
2025-11-06 22:35:33 +0530

    backend files

---
+++ backed/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoutes.js"
+import resultRouter from "./routes/resultRoute.js"
+
+const app = express()
+const PORT = 4000
+
+// MIDDLEWARE 
+app.use(cors())
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+
+
+// DB
+connectDB();
+
+// ROUTES
+app.use('/api/auth', userRouter)
+app.use('/api/results', resultRouter)
+
+app.get('/', (req, res) => {
+    res.send('API WORKING')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on: http://localhost:${PORT}`)
+})
+
