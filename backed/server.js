86f09967df77501276095bb8d949124a86a9ccf4
Shreyash <cyberninjax45@gmail.com>
2025-11-06 22:41:17 +0530

    server.js

--- backed/server.js
+++ backed/server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoutes.js"
-import resultRouter from "./routes/resultRoute.js"
+
 
 const app = express()
 const PORT = 4000
@@ -28,4 +28,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on: http://localhost:${PORT}`)
 })
+
 
