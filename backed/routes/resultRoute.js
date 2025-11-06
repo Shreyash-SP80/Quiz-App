bb7d16a9b2897451c15efb7ad25b38e024c520ae
Shreyash <cyberninjax45@gmail.com>
2025-11-06 22:42:05 +0530

    resultRoute.js

--- backed/routes/resultRoute.js
+++ backed/routes/resultRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
 import { createResult, listResult } from "../controllers/resultController.js"
+// import { createResult, listResult } from "../controllers/resultController.js"
 
 const resultRouter = express.Router()
 
 resultRouter.post("/", authMiddleware, createResult)
 resultRouter.get("/", authMiddleware, listResult)
+
 
 export default resultRouter
